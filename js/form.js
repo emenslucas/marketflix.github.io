c086d020de568aabc31473fe43a88519548d7afd
Facundo Neiman <115724830+FacundoNeiman@users.noreply.github.com>
2023-05-19 20:49:18 -0300

    feat: add alert after send

--- js/form.js
+++ js/form.js
@@ -1,3 +1,11 @@
-function alerta() {
-  alert("Mensaje enviado! Te estaremos contactando pronto.");
+window.onload = function () {
+  var formSubmitted = sessionStorage.getItem("formSubmitted");
+  if (formSubmitted === "true") {
+    alert("¡El formulario ha sido enviado correctamente!");
+    sessionStorage.removeItem("formSubmitted");
+  }
+};
+
+function setFormSubmitted() {
+  sessionStorage.setItem("formSubmitted", "true");
 }
