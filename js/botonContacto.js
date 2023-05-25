e91c80dfc8c03ece3853bf1d120c874e704e3a54
Facundo Neiman <115724830+FacundoNeiman@users.noreply.github.com>
2023-05-25 19:48:41 -0300

    fix

--- js/botonContacto.js
+++ js/botonContacto.js
@@ -1,7 +1,10 @@
-var contactoBtn = document.getElementById("contacto-btn");
+document.addEventListener("DOMContentLoaded", function () {
+  // Obtén el botón de "Contáctanos"
+  var contactoBtn = document.getElementById("contacto-btn");
 
-// Agrega un controlador de evento para el clic en el botón
-contactoBtn.addEventListener("click", function () {
-  // Redirige a index.html y agrega el fragmento "#contacto" en la URL
-  window.location.href = "index.html#contacto";
+  // Agrega un controlador de evento para el clic en el botón
+  contactoBtn.addEventListener("click", function () {
+    // Redirige a index.html y agrega el fragmento "#contacto" en la URL
+    window.location.href = "index.html#contacto";
+  });
 });
